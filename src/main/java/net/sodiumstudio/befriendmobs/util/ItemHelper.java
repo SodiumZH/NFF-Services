7d2c44a4da40859fc8c44ee4bc6cc44fbf716fb6
SodiumZH <hlzhang_1998@outlook.com>
2023-05-12 23:56:28 +0800

    made bauble system key-sensitive

--- src/main/java/net/sodiumstudio/befriendmobs/util/ItemHelper.java
+++ src/main/java/net/sodiumstudio/befriendmobs/util/ItemHelper.java
@@ -1,10 +1,11 @@
 package net.sodiumstudio.befriendmobs.util;
 
-import net.minecraft.world.entity.player.Inventory;
+import java.util.function.Predicate;
+
+import net.minecraft.core.Registry;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 
 public class ItemHelper
 {
@@ -34,6 +35,4 @@ public class ItemHelper
 		return false;
 	}
 
-
-	
 }
