166caf8b19db40dab778d908e814cabbe1c04c40
SodiumZH <hlzhang_1998@outlook.com>
2023-05-29 19:19:51 +0800

    Update ReflectHelper.java

--- src/main/java/net/sodiumstudio/befriendmobs/util/ReflectHelper.java
+++ src/main/java/net/sodiumstudio/befriendmobs/util/ReflectHelper.java
@@ -9,24 +9,27 @@ public class ReflectHelper
 	/**
 	 * Force get a non-public field value.
 	 * @param obj Target object.
-	 * @param objClass Class in which the field is defined. (Not always equals to {code obj.class}!)
+	 * @param declaredClass Class in which the field is defined. (Not always equals to {code obj.class}!)
 	 * @param fieldName Field to get.
+	 * @param noStackTrace If true, it will not print stack trace if exception thrown.
 	 * @param value New value to get.
 	 * @return Value got.
 	 */
-	public static Object forceGet(Object obj, Class<?> objClass, String fieldName)
+	public static Object forceGet(Object obj, Class<?> declaredClass, String fieldName, boolean noStackTrace)
 	{
 		Object result = null;
 		try
 		{
-		Field fld = objClass.getDeclaredField(fieldName);
+		Field fld = declaredClass.getDeclaredField(fieldName);
 		fld.setAccessible(true);
 		result = fld.get(obj);
 		fld.setAccessible(false);
 		}
 		catch(Exception e)
 		{
-			e.printStackTrace();
+			if (!noStackTrace)
+				e.printStackTrace();
+			return null;
 		}
 		return result;
 	}
@@ -34,20 +37,34 @@ public class ReflectHelper
 	/**
 	 * Force get a non-public field value.
 	 * @param obj Target object.
-	 * @param objClass Class in which the field is defined. (Not always equals to {code obj.class}!)
+	 * @param declaredClass Class in which the field is defined. (Not always equals to {code obj.class}!)
+	 * @param fieldName Field to get.
+	 * @param value New value to get.
+	 * @return Value got.
+	 */
+	public static Object forceGet(Object obj, Class<?> declaredClass, String fieldName)
+	{
+		return forceGet(obj, declaredClass, fieldName, false);
+	}
+	
+	/**
+	 * Force get a non-public field value.
+	 * @deprecated use {@code forceGet} instead.
+	 * @param obj Target object.
+	 * @param declaredClass Class in which the field is defined. (Not always equals to {code obj.class}!)
 	 * @param fieldName Field to get.
 	 * @param value New value to get.
 	 * @return Value got.
 	 */
 	@Deprecated
 	@SuppressWarnings("unchecked")
-	public static <T> T forceGetCasted(Object obj, Class<?> objClass, String fieldName)
+	public static <T> T forceGetCasted(Object obj, Class<?> declaredClass, String fieldName)
 	{
 		Object result = null;
 		T resultCast = null;
 		try
 		{
-		Field fld = objClass.getDeclaredField(fieldName);
+		Field fld = declaredClass.getDeclaredField(fieldName);
 		fld.setAccessible(true);
 		result = fld.get(obj);
 		fld.setAccessible(false);
@@ -63,69 +80,113 @@ public class ReflectHelper
 	/**
 	 * Force set a non-public field value
 	 * @param obj Target object.
-	 * @param objClass Class in which the field is defined. (Not always equals to {code obj.class}!)
+	 * @param declaredClass Class in which the field is defined. (Not always equals to {code obj.class}!)
 	 * @param fieldName Field to set.
+	 * @param noStackTrace If true, it will not print stack trace if exception thrown.
 	 * @param value New value to set.
 	 */
-	public static void forceSet(Object obj, Class<?> objClass, String fieldName, Object value)
+	public static void forceSet(Object obj, Class<?> declaredClass, String fieldName, Object value, boolean noStackTrace)
 	{
 		try
 		{
-		Field fld = objClass.getDeclaredField(fieldName);
+		Field fld = declaredClass.getDeclaredField(fieldName);
 		fld.setAccessible(true);
 		fld.set(obj, value);
 		fld.setAccessible(false);
 		}
 		catch(Exception e)
 		{
-			e.printStackTrace();
+			if (!noStackTrace)
+				e.printStackTrace();
 		}
+	}
+	
+	/**
+	 * Force set a non-public field value
+	 * @param obj Target object.
+	 * @param declaredClass Class in which the field is defined. (Not always equals to {code obj.class}!)
+	 * @param fieldName Field to set.
+	 * @param value New value to set.
+	 */
+	public static void forceSet(Object obj, Class<?> declaredClass, String fieldName, Object value)
+	{
+		forceSet(obj, declaredClass, fieldName, value, false);
 	}
 	
 	/**
 	 * Force invoke a non-public method without return value.
 	 * @param obj Target object.
-	 * @param objClass Class in which the method is defined. (Not always equals to {code obj.class}!)
+	 * @param declaredClass Class in which the method is defined. (Not always equals to {@code obj.class}!)
+	 * @param noStackTrace If true, it will not print stack trace if exception thrown.
 	 * @param methodName Method to run.
 	 * @param params Method parameters.
 	 */
-	public static void forceInvoke(Object obj, Class<?> objClass, String methodName, Object... params)
+	public static void forceInvoke(Object obj, Class<?> declaredClass, boolean noStackTrace, String methodName, Object... params)
 	{
 		try
 		{
-		Method method = objClass.getDeclaredMethod(methodName);
+		Method method = declaredClass.getDeclaredMethod(methodName);
 		method.setAccessible(true);
 		method.invoke(obj, params);
 		method.setAccessible(false);
 		}
 		catch(Exception e)
 		{
-			e.printStackTrace();
+			if (!noStackTrace)
+				e.printStackTrace();
 		}
+	}
+	
+	/**
+	 * Force invoke a non-public method without return value.
+	 * @param obj Target object.
+	 * @param declaredClass Class in which the method is defined. (Not always equals to {@code obj.class}!)
+	 * @param methodName Method to run.
+	 * @param params Method parameters.
+	 */
+	public static void forceInvoke(Object obj, Class<?> declaredClass, String methodName, Object... params)
+	{
+		forceInvoke(obj, declaredClass, false, methodName, params);
 	}
 	
 	/**
 	 * Force invoke a non-public method with return value.
 	 * @param obj Target object.
-	 * @param objClass Class in which the method is defined. (Not always equals to {code obj.class}!)
+	 * @param declaredClass Class in which the method is defined. (Not always equals to {@code obj.class}!)
+	 * @param noStackTrace If true, it will not print stack trace if exception thrown.
 	 * @param fieldName Field to set.
 	 * @param params Method parameters.
 	 * @return Returned value.
 	 */
-	public static Object forceInvokeRetVal(Object obj, Class<?> objClass, String methodName, Object... params)
+	public static Object forceInvokeRetVal(Object obj, Class<?> declaredClass, boolean noStackTrace, String methodName, Object... params)
 	{
 		Object result = null;
 		try
 		{
-		Method method = objClass.getDeclaredMethod(methodName);
+		Method method = declaredClass.getDeclaredMethod(methodName);
 		method.setAccessible(true);
 		result = method.invoke(obj, params);
 		method.setAccessible(false);
 		}
 		catch(Exception e)
 		{
-			e.printStackTrace();
+			if (!noStackTrace)
+				e.printStackTrace();
+			return null;
 		}
 		return result;
+	}
+
+	/**
+	 * Force invoke a non-public method with return value.
+	 * @param obj Target object.
+	 * @param declaredClass Class in which the method is defined. (Not always equals to {@code obj.class}!)
+	 * @param fieldName Field to set.
+	 * @param params Method parameters.
+	 * @return Returned value.
+	 */
+	public static Object forceInvokeRetVal(Object obj, Class<?> declaredClass, String methodName, Object... params)
+	{
+		return forceInvokeRetVal(obj, declaredClass, false, methodName, params);
 	}
 }
