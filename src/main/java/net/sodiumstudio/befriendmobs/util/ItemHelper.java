9d4d57fcc8cded34df374a9e33d45ecf10d53ada
SodiumZH <hlzhang_1998@outlook.com>
2023-07-02 23:53:26 +0800

    Refactored Bauble Items
    Used registry key instead of item object for compat
    Also added some utilities about registry keys

--- src/main/java/net/sodiumstudio/befriendmobs/util/ItemHelper.java
+++ src/main/java/net/sodiumstudio/befriendmobs/util/ItemHelper.java
@@ -1,15 +1,18 @@
 package net.sodiumstudio.befriendmobs.util;
 
-import java.util.function.Predicate;
-
-import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class ItemHelper
 {
 	
+	/** @deprecated use {@link ItemStack#shrink} instead
+	 * 
+	*/
+	@Deprecated
 	public static void consumeOne(ItemStack stack)
 	{
 		if (stack.isEmpty())
@@ -18,11 +21,17 @@ public class ItemHelper
 		stack.setCount(amount - 1);
 	}
 	
+	/**
+	 * Check if player has the input item on either hand.
+	 */
 	public static boolean hasItemInHand(Player player, Item item)
 	{
 		return player.getMainHandItem().is(item) || player.getOffhandItem().is(item);
 	}
 	
+	/**
+	 * Check if player has any of the input items on either hand.
+	 */
 	public static boolean hasItemInHand(Player player, Item[] items)
 	{
 		for (Item item : items)
@@ -35,4 +44,78 @@ public class ItemHelper
 		return false;
 	}
 
+	/**
+	 * Get item registry key as ResourceLocation.
+	 */	
+	public static ResourceLocation getRegistryKey(Item item)
+	{
+		return ForgeRegistries.ITEMS.getKey(item);
+	}
+
+	/**
+	 * Get item registry key as ResourceLocation, from ItemStack.
+	 */	
+	public static ResourceLocation getRegistryKey(ItemStack stack)
+	{
+		return getRegistryKey(stack.getItem());
+	}
+	
+	/**
+	 * Get item registry key as "domain:name" formatted string.
+	 */	
+	public static String getRegistryKeyStr(Item item)
+	{
+		return getRegistryKey(item).toString();
+	}
+
+	/**
+	 * Get item registry key as "domain:name" formatted string, from ItemStack.
+	 */	
+	public static String getRegistryKeyStr(ItemStack stack)
+	{
+		return getRegistryKey(stack).toString();
+	}
+	
+	/**
+	 * Get Item object from registry name. If the registry name doesn't exist, return null.
+	 */
+	public static Item getItem(ResourceLocation registryKey)
+	{
+		return ForgeRegistries.ITEMS.getValue(registryKey);
+	}
+
+	/**
+	 * Get Item object from registry name. If the registry name doesn't exist, return null.
+	 * Use "domain:name" format for string key.
+	 */
+	public static Item getItem(String registryKey)
+	{
+		return getItem(new ResourceLocation(registryKey));
+	}
+	
+	/**
+	 * Get Item object from registry name. If the registry name doesn't exist, return null.
+	 */
+	public static Item getItem(String domain, String name)
+	{
+		return getItem(new ResourceLocation(domain, name));
+	}
+	
+	/**
+	 * Check if an ItemStack is instance of k given item, from item's registry key.
+	 */
+	public static boolean is(ItemStack stack, ResourceLocation registryKey)
+	{
+		return stack.is(ForgeRegistries.ITEMS.getValue(registryKey));
+	}
+	
+	public static boolean is(ItemStack stack, String registryKey)
+	{
+		return is(stack, new ResourceLocation(registryKey));
+	}
+	public static boolean is(ItemStack stack, String domain, String name)
+	{
+		return is(stack, new ResourceLocation(domain, name));
+	}
+	
 }
---
+++ src/main/java/net/sodiumstudio/befriendmobs/util/containers/MapPair.java
@@ -0,0 +1,35 @@
+package net.sodiumstudio.befriendmobs.util.containers;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class MapPair <K, V>
+{
+	@Nonnull
+	protected K k;
+	@Nullable
+	protected V v;
+	
+	private MapPair(K key, V value)
+	{
+		if (key == null)
+			throw new IllegalArgumentException("MapPair key cannot be null.");
+		this.k = key;
+		this.v = value;
+	}
+	
+	public K getK()
+	{
+		return k;
+	}
+	
+	public V getV()
+	{
+		return v;
+	}
+	
+	public static <K, V> MapPair<K, V> of(K k, V v)
+	{
+		return new MapPair<K, V>(k, v);
+	}
+}
