6bb3b262e9e7f1148b26dee7bb2794034db5e443
SodiumZH <hlzhang_1998@outlook.com>
2023-08-10 17:13:50 +0800

    Added CBMPlayer One-tick tag
    nbt tags that are auto-cleared on tick end

--- src/main/java/net/sodiumstudio/befriendmobs/events/ServerEvents.java
+++ src/main/java/net/sodiumstudio/befriendmobs/events/ServerEvents.java
@@ -2,12 +2,15 @@ package net.sodiumstudio.befriendmobs.events;
 
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.common.Mod;
 import net.sodiumstudio.befriendmobs.BefriendMobs;
+import net.sodiumstudio.befriendmobs.entity.capability.CBMPlayer;
 
 @Mod.EventBusSubscriber(modid = BefriendMobs.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ServerEvents 
@@ -37,4 +40,16 @@ public class ServerEvents
 		}
 	}
 
+	@SubscribeEvent(priority = EventPriority.LOWEST)
+	public static void onWorldTickFinalize(TickEvent.LevelTickEvent event)
+	{
+		if (event.phase == TickEvent.Phase.END && event.side == LogicalSide.SERVER)
+		{
+			for (Player player: event.level.players())
+			{
+				CBMPlayer.removeOneTickTags(player);
+			}
+		}
+	}
+	
 }
