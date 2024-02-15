04c6576e7104278e7664231f3b9a24a6eec79940
SodiumZH <hlzhang_1998@outlook.com>
2024-02-16 01:27:52 +0800

    thrown trident damage events

---
+++ src/main/java/net/sodiumstudio/nautils/events/ThrownTridentSetBaseDamageEvent.java
@@ -0,0 +1,30 @@
+package net.sodiumstudio.nautils.events;
+
+import net.minecraft.world.entity.projectile.ThrownTrident;
+
+public class ThrownTridentSetBaseDamageEvent extends NaUtilsEntityEvent<ThrownTrident>
+{
+	private float dmg;
+	private final float originalDmg;
+	public ThrownTridentSetBaseDamageEvent(ThrownTrident entity, float originalDmg)
+	{
+		super(entity);
+		this.dmg = originalDmg;
+		this.originalDmg = originalDmg;
+	}
+	
+	public float getDamage()
+	{
+		return dmg;
+	}
+	
+	public void setDamage()
+	{
+		this.dmg = dmg;
+	}
+	
+	public float getOriginalDamage()
+	{
+		return originalDmg;
+	}
+}
