69b35fdc9d8a199aa08da0ed91ceb47cdcc43d90
SodiumZH <hlzhang_1998@outlook.com>
2024-02-17 02:37:41 +0800

    fix mixin

--- src/main/java/net/sodiumstudio/nautils/events/NaUtilsLivingEvent.java
+++ src/main/java/net/sodiumstudio/nautils/events/NaUtilsLivingEvent.java
@@ -4,7 +4,7 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraftforge.event.entity.living.LivingEvent;
 
 /**
- * An utility class as a templated {@link LivingEvent) that can automatically cast the entity to given class.
+ * An utility class as a templated {@link LivingEvent} that can automatically cast the entity to given class.
  */
 public class NaUtilsLivingEvent<T extends LivingEntity> extends LivingEvent
 {
