bb7cc0090aa12abb89096a84d2684f604baa04ee
SodiumZH <hlzhang_1998@outlook.com>
2024-03-10 16:54:25 +0800

    DebugTargetSetter

--- src/main/java/net/sodiumstudio/nautils/registries/NaUtilsItemRegistry.java
+++ src/main/java/net/sodiumstudio/nautils/registries/NaUtilsItemRegistry.java
@@ -6,6 +6,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.sodiumstudio.nautils.NaUtils;
 import net.sodiumstudio.nautils.item.debug.DebugAISwitchItem;
+import net.sodiumstudio.nautils.item.debug.DebugTargetSetterItem;
 
 public class NaUtilsItemRegistry
 {
@@ -13,5 +14,6 @@ public class NaUtilsItemRegistry
 	// TODO: NaUtils.MOD_ID
 	public static final DeferredRegister<Item> NAUTILS_ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, NaUtils.MOD_ID);
 	
-	public static final RegistryObject<Item> DEBUG_AI_SWITCH = NAUTILS_ITEMS.register("debug_ai_switch", () -> new DebugAISwitchItem(new Item.Properties().stacksTo(1)));
+	public static final RegistryObject<DebugAISwitchItem> DEBUG_AI_SWITCH = NAUTILS_ITEMS.register("debug_ai_switch", () -> new DebugAISwitchItem(new Item.Properties().stacksTo(1)));
+	public static final RegistryObject<DebugTargetSetterItem> DEBUG_TARGET_SETTER = NAUTILS_ITEMS.register("debug_target_setter", () -> new DebugTargetSetterItem(new Item.Properties().stacksTo(1)));
 }
