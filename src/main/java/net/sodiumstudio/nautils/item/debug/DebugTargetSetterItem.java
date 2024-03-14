3629ddf146634d8ec8ab7c39872161f52166204f
SodiumZH <hlzhang_1998@outlook.com>
2024-03-15 00:48:29 +0800

    DebugTargetSetterItem

--- src/main/java/net/sodiumstudio/nautils/item/debug/DebugTargetSetterItem.java
+++ src/main/java/net/sodiumstudio/nautils/item/debug/DebugTargetSetterItem.java
@@ -18,8 +18,13 @@ import net.sodiumstudio.nautils.EntityHelper;
 import net.sodiumstudio.nautils.InfoHelper;
 import net.sodiumstudio.nautils.NaMiscUtils;
 import net.sodiumstudio.nautils.NbtHelper;
+import net.sodiumstudio.nautils.item.NaUtilsItem;
 
-public class DebugTargetSetterItem extends Item
+/**
+ * NOT IMPLEMENTED!!!!
+ */
+//@Deprecated
+public class DebugTargetSetterItem extends NaUtilsItem
 {
 	public DebugTargetSetterItem(Properties pProperties)
 	{
@@ -30,11 +35,10 @@ public class DebugTargetSetterItem extends Item
 	@Nullable
 	private Mob getAttacker(ItemStack stack, Level level)
 	{
-		stack.getOrCreateTag();
 		if (level.isClientSide)
 			return null;
 		Mob attacker = null;
-		if (stack.getTag().hasUUID("attacker"))
+		if (stack.getOrCreateTag().hasUUID("attacker"))
 		{
 			Entity attackerEntity = EntityHelper.getEntityByUUID(level, stack.getTag().getUUID("attacker"));
 			if (attackerEntity != null && attackerEntity instanceof Mob m)
@@ -48,87 +52,87 @@ public class DebugTargetSetterItem extends Item
 		if (attacker == null)
 			stack.getOrCreateTag().remove("attacker");
 		else 
-			stack.getTag().putUUID("attacker", attacker.getUUID());
+			stack.getOrCreateTag().putUUID("attacker", attacker.getUUID());
 	}
 	
 	@Override
-	public InteractionResult interactLivingEntity(ItemStack stack, Player player, LivingEntity target, InteractionHand hand) 
+	public InteractionResult interactLivingEntity(Player player, LivingEntity target, InteractionHand hand) 
 	{
-		stack.getOrCreateTag();
-		if (!player.level.isClientSide)
+		Mob attacker = getAttacker(player.getItemInHand(hand), player.level);
+		// If targeting a mob attacking the user player, remove the target and reset
+		if (player.level.isClientSide)
+			return InteractionResult.SUCCESS;
+		if (target instanceof Mob mob && mob.getTarget() == player)
 		{
-			Mob attacker = getAttacker(stack, player.level);
-			// If targeting a mob attacking the user player, remove the target and reset item
-			if (target instanceof Mob mob && mob.getTarget() == player)
+			attacker.setTarget(null);
+			NaMiscUtils.printToScreen("Removed the target of [" + mob.getName().getString() + "].", player);
+			if (attacker != null)
 			{
-				attacker.setTarget(null);
-				NaMiscUtils.printToScreen("Removed the target of " + mob.getName(), player);
-				if (attacker != null)
-				{
-					setAttacker(stack, null);
-					NaMiscUtils.printToScreen("Target Setter reset", player);
-				}
+				setAttacker(player.getItemInHand(hand), null);
+				NaMiscUtils.printToScreen("Target Setter reset.", player);
+			}
+			return InteractionResult.sidedSuccess(player.level.isClientSide);
+		}
+		// If no attacker, set attacker first
+		else if (attacker == null)
+		{
+			if (target instanceof Mob m)
+			{
+				setAttacker(player.getItemInHand(hand), m);
+				NaMiscUtils.printToScreen("Setting the target of [" + m.getName().getString() + "]...", player);
 				return InteractionResult.sidedSuccess(player.level.isClientSide);
 			}
-			// If no attacker, set attacker first
-			else if (attacker == null) 
+		} else
+		{
+			// If re-click the attack, remove the target
+			if (target == attacker)
 			{
-				if (target instanceof Mob m)
+				if (attacker.getTarget() != null)
 				{
-					setAttacker(stack, m);
-					NaMiscUtils.printToScreen("Set attacker to " + m.getName(), player);
+					attacker.setTarget(null);
+					NaMiscUtils.printToScreen("Removed the target of [" + attacker.getName().getString() + "].", player);
+					return InteractionResult.sidedSuccess(player.level.isClientSide);
+				} else
+				{
+					NaMiscUtils.printToScreen("Setting the target of [" + attacker.getName().getString() + "]...", player);
 					return InteractionResult.sidedSuccess(player.level.isClientSide);
 				}
 			}
+			// Otherwise set target
 			else
 			{
-				// If re-click the attack, remove the target
-				if (target == attacker)
-				{
-					if (attacker.getTarget() != null)
-					{
-						attacker.setTarget(null);
-						NaMiscUtils.printToScreen("Removed the target of " + attacker.getName(), player);
-						return InteractionResult.sidedSuccess(player.level.isClientSide);
-					}
-					else return InteractionResult.PASS;
-				}
-				// Otherwise set target and reset the item
-				else
-				{
-					attacker.setTarget(target);
-					NaMiscUtils.printToScreen("Set the target of " + attacker.getName() + " to " + target.getName(), player);
-					setAttacker(stack, null);
-					return InteractionResult.sidedSuccess(player.level.isClientSide);
-				}
+				attacker.setTarget(target);
+				NaMiscUtils.printToScreen(
+						"Set the target of [" + attacker.getName().getString() + "] to [" + target.getName().getString() + "].",
+						player);
+				return InteractionResult.sidedSuccess(player.level.isClientSide);
 			}
 		}
 		return InteractionResult.PASS;
 	}
-
+	
 	@Override
 	public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand)
 	{
-		ItemStack stack= player.getItemInHand(hand);
+		ItemStack stack = player.getItemInHand(hand);
+		if (player.level.isClientSide) 
+			return InteractionResultHolder.success(stack);
 		Mob mob = getAttacker(stack, player.level);
 		if (mob != null)
 		{
 			if (!player.isShiftKeyDown())
 			{
-				setAttacker(stack, null);
-				NaMiscUtils.printToScreen("Target Setter reset", player);
+				setAttacker(player.getItemInHand(hand), null);
+				NaMiscUtils.printToScreen("Target Setter reset.", player);
 				return InteractionResultHolder.sidedSuccess(stack, player.level.isClientSide);
 			}
 			else
 			{
 				mob.setTarget(player);
-				NaMiscUtils.printToScreen("Set the target of " + mob.getName() + " to self (" + player.getName() + ")!", player);
+				NaMiscUtils.printToScreen("Set the target of [" + mob.getName().getString() + "] to self (" + player.getName().getString() + ")!", player);
 				return InteractionResultHolder.sidedSuccess(stack, player.level.isClientSide);
 			}
 		}
 		return InteractionResultHolder.pass(stack);
-			
-		
-		
 	}
 }
