fe818d1f441daee3955459bbc626ebb02a287486
SodiumZH <hlzhang_1998@outlook.com>
2024-03-15 00:48:29 +0800

    DebugTargetSetterItem

--- src/main/java/net/sodiumstudio/nautils/registries/NaUtilsItemRegistry.java
+++ src/main/java/net/sodiumstudio/nautils/registries/NaUtilsItemRegistry.java
@@ -1,6 +1,7 @@
 package net.sodiumstudio.nautils.registries;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -14,6 +15,6 @@ public class NaUtilsItemRegistry
 	// TODO: NaUtils.MOD_ID
 	public static final DeferredRegister<Item> NAUTILS_ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, NaUtils.MOD_ID);
 	
-	public static final RegistryObject<DebugAISwitchItem> DEBUG_AI_SWITCH = NAUTILS_ITEMS.register("debug_ai_switch", () -> new DebugAISwitchItem(new Item.Properties().stacksTo(1)));
-	public static final RegistryObject<DebugTargetSetterItem> DEBUG_TARGET_SETTER = NAUTILS_ITEMS.register("debug_target_setter", () -> new DebugTargetSetterItem(new Item.Properties().stacksTo(1)));
+	public static final RegistryObject<DebugAISwitchItem> DEBUG_AI_SWITCH = NAUTILS_ITEMS.register("debug_ai_switch", () -> new DebugAISwitchItem(new Item.Properties().stacksTo(1).rarity(Rarity.EPIC)));
+	public static final RegistryObject<DebugTargetSetterItem> DEBUG_TARGET_SETTER = NAUTILS_ITEMS.register("debug_target_setter", () -> new DebugTargetSetterItem(new Item.Properties().stacksTo(1).rarity(Rarity.EPIC)));
 }
