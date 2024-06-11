6f015758e0985c314943d728e8fbcb34e4e945e6
SodiumZH <hlzhang_1998@outlook.com>
2024-06-11 20:02:40 +0800

    Update NaGuiUtils.java

--- src/main/java/net/sodiumstudio/nautils/client/NaGuiUtils.java
+++ src/main/java/net/sodiumstudio/nautils/client/NaGuiUtils.java
@@ -1,9 +1,8 @@
 package net.sodiumstudio.nautils.client;
 
 import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.PoseStack;
 
-import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.resources.ResourceLocation;
 import net.sodiumstudio.nautils.math.GuiPos;
@@ -23,8 +22,9 @@ public class NaGuiUtils
 	 * @param uvOffset UV (XY) offset on texture (i.e. UV/XY position of the top-left corner of the sprite on texture)
 	 * @param uvSize Sprite UV/XY size.
 	 */
-	public static void drawSprite(PoseStack GuiGraphics, GuiPos xy, int blitOffset, GuiPos uvOffset, GuiPos uvSize, GuiPos textureSize)
+	public static void drawSprite(GuiGraphics graphics, ResourceLocation atlasLocation, 
+			GuiPos xy, int blitOffset, GuiPos uvOffset, GuiPos uvSize, GuiPos textureSize)
 	{
-		GuiComponent.blit(GuiGraphics, xy.x, xy.y, blitOffset, uvOffset.x, uvOffset.y, uvSize.x, uvSize.y, textureSize.x, textureSize.y);
+		graphics.blit(atlasLocation, xy.x, xy.y, blitOffset, uvOffset.x, uvOffset.y, uvSize.x, uvSize.y, textureSize.x, textureSize.y);
 	}
 }
