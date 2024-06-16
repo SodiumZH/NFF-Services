8e02161f0665ebed36432b959eb712bc6e91154a
SodiumZH <hlzhang_1998@outlook.com>
2024-06-17 00:45:20 +0800

    Fix RepeatableAttributeModifier

--- src/main/java/net/sodiumstudio/nautils/entity/RepeatableAttributeModifier.java
+++ src/main/java/net/sodiumstudio/nautils/entity/RepeatableAttributeModifier.java
@@ -11,6 +11,8 @@ import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 /**
  * A {@code RepeatableAttributeModifier} represents an attribute modifier which can be applied multiple times.
  * It's recommended to create a static instance for each {@code RepeatableAttributeModifier} in each class.
+ * <p>Note: The complexity of the {@code apply} operation is O(times) because it must iterate through the whole modifier list
+ * to find which modifier it's previously applying. Take care if you need to update on tick.
  */
 public class RepeatableAttributeModifier
 {
@@ -37,11 +39,12 @@ public class RepeatableAttributeModifier
 	
 	public AttributeModifier get(int index)
 	{
-		if (index >= this.maxSize)
+		if (index > this.maxSize)
 			throw new IllegalArgumentException("Index is larger than the set max. Attempted index: " + Integer.toString(index) + "; Set max: " + Integer.toString(maxSize));
-		while (modifiers.size() <= index)
+		// The first element (index == 0) is zero, so max length should be (max + 1).
+		while (modifiers.size() <= index + 1)
 		{
-			modifiers.add(new AttributeModifier(UUID.randomUUID(), Integer.toString(modifiers.size()), this.value * (index + 1), this.operation));
+			modifiers.add(new AttributeModifier(UUID.randomUUID(), Integer.toString(modifiers.size()), this.value * index, this.operation));
 		}
 		return modifiers.get(index);
 	}
@@ -49,19 +52,26 @@ public class RepeatableAttributeModifier
 	public void apply(LivingEntity target, Attribute attribute, int times, boolean isPermanent)
 	{
 		AttributeInstance inst = target.getAttribute(attribute);
-		int i = 0;
+		// Check if it's already applying the same modifier. This could prevent iteration on tick.
+		if (inst.hasModifier(this.get(times)))
+		{
+			inst.removeModifier(this.get(times));
+			if (isPermanent)
+				inst.addPermanentModifier(this.get(times));
+			else inst.addTransientModifier(this.get(times));
+			return;
+		}
 		for (var modifier: modifiers)
 		{
 			inst.removeModifier(modifier);
 		}
 		if (isPermanent)
 		{
-			inst.addPermanentModifier(this.get(i));
+			inst.addPermanentModifier(this.get(times));
 		}
-		else inst.addTransientModifier(this.get(i));
+		else inst.addTransientModifier(this.get(times));
 	}
-	
-	
+
 	public void clear(LivingEntity target, Attribute attribute)
 	{
 		AttributeInstance inst = target.getAttribute(attribute);
