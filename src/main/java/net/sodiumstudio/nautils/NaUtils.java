fbf736d68901c18405a620618a4e37ceb216ea3c
SodiumZH <hlzhang_1998@outlook.com>
2024-06-26 01:19:17 +0800

    Update NaUtils.java
    record server

--- src/main/java/net/sodiumstudio/nautils/NaUtils.java
+++ src/main/java/net/sodiumstudio/nautils/NaUtils.java
@@ -1,6 +1,13 @@
 package net.sodiumstudio.nautils;
 
+import com.mojang.logging.LogUtils;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.event.server.ServerAboutToStartEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.event.server.ServerStoppedEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.sodiumstudio.befriendmobs.registry.BMItems;
 import net.sodiumstudio.nautils.registries.NaUtilsItemRegistry;
@@ -10,6 +17,7 @@ public class NaUtils
 	
 	// TODO: change to "nautils" after separation
 	public static final String MOD_ID = "befriendmobs";
+	private static MinecraftServer server;
 	
 	public NaUtils()
 	{
@@ -18,5 +26,22 @@ public class NaUtils
 		NaUtilsItemRegistry.NAUTILS_ITEMS.register(modEventBus);
 	}
 
+	public static MinecraftServer getServer()
+	{
+		return server;
+	}
+	
+    @SubscribeEvent
+    public static void onServerAboutToStart(ServerAboutToStartEvent event)
+    {
+        server = event.getServer();
+    }
+    
+    @SubscribeEvent
+    public static void onServerStopped(ServerStoppedEvent event)
+    {
+    	server = null;
+    }
+	
 	
 }
