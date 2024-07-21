f5e8014493c4c653162c6f40600eaa7e43dc800a
SodiumZH <hlzhang_1998@outlook.com>
2024-07-21 22:31:02 +0800

    Caps + Sync
    Added CEntityTickingCapability, can be auto ticked
    Added some cap provider preset
    Added NaUtilsSynchableDataType. It defines data types that can be serialized/deserialized using both buf and nbt.

---
+++ src/main/java/net/sodiumstudio/nautils/capability/NaUtilsCapsEventListeners.java
@@ -0,0 +1,21 @@
+package net.sodiumstudio.nautils.capability;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.sodiumstudio.nautils.events.entity.EntityTickEvent;
+
+/**
+ * Event listeners for NaUtils capability implementation.
+ */
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class NaUtilsCapsEventListeners
+{
+	@SubscribeEvent
+	public static void onEntityTick(EntityTickEvent event)
+	{
+		for (var cap: CEntityTickingCapability.ALL_CAPS)
+		{
+			event.getEntity().getCapability(cap).ifPresent(CEntityTickingCapability::tick);
+		}
+	}
+}
