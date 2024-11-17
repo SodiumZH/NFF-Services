c73f3459d6e3ea413c05bd8e6ec0fdcf4608f642
SodiumZH <hlzhang_1998@outlook.com>
2024-11-17 22:58:51 +0800

    NaUtilItems

--- src/main/java/net/sodiumzh/nautils/statics/NaUtilsMiscStatics.java
+++ src/main/java/net/sodiumzh/nautils/statics/NaUtilsMiscStatics.java
@@ -1,6 +1,7 @@
 package net.sodiumzh.nautils.statics;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -9,11 +10,13 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.common.util.NonNullSupplier;
+import net.minecraftforge.registries.IForgeRegistry;
 import org.apache.commons.lang3.mutable.MutableObject;
 
 
@@ -183,5 +186,12 @@ public class NaUtilsMiscStatics {
 				return true;
 		}
 		return false;
+	/**
+	 * Get entry from Forge registry if present, otherwise return empty instead of registry default value.
+	 */
+	public static <T> Optional<T> getEntryOptional(IForgeRegistry<T> reg, ResourceLocation key)
+	{
+		if (reg.containsKey(key)) return Optional.ofNullable(reg.getValue(key));
+		else return Optional.empty();
 	}
 }
