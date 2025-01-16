4d9a2e22603fb8111d89fb53f0f06a8d2bbae53d
SodiumZH <46860829+SodiumZH@users.noreply.github.com>
2025-01-17 00:32:42 +0800

    Vanilla taming process
    Trying to apply nff taming process to vanilla tamable animal

---
+++ src/main/java/net/sodiumzh/nautils/entity/taming/ITamingProcess.java
@@ -0,0 +1,149 @@
+package net.sodiumzh.nautils.entity.taming;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.player.Player;
+import net.sodiumzh.nautils.entity.anger.MobAngerReason;
+import net.sodiumzh.nautils.entity.anger.MobAngerRules;
+import net.sodiumzh.nautils.math.RandomSelection;
+import net.sodiumzh.nautils.statics.NaUtilsEntityStatics;
+import net.sodiumzh.nautils.statics.NaUtilsMathStatics;
+import net.sodiumzh.nff.services.entity.taming.TamableInteractArguments;
+
+import javax.annotation.Nullable;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+/**
+ * Base interface of all taming processes.
+ * @param <T>The tamable mob's base class.
+ */
+public interface ITamingProcess<T extends Mob> {
+
+    /**
+     * Tame the mob.
+     */
+    public Mob doTaming(Player player, T target);
+
+    public TamingInteractionResult handleInteract(Player player, T target, InteractionHand hand);
+
+    /**
+     * Invoked on mob tick on server.
+     */
+    public void serverTick(T mob);
+
+    /**
+     * Invoked when a taming process should be aborted and reset.
+     * @param player Target player.
+     * @param mob Target mob.
+     * @param isQuiet If true, it should not send signals (like particles).
+     */
+    public void interrupt(Player player, T mob, boolean isQuiet);
+
+    /**
+     * Interrupt all players' processes.
+     */
+    public default boolean interruptAll(T mob, boolean isQuiet)
+    {
+        boolean res = false;
+        for (Player player: mob.level().players())
+        {
+            if (player != null && isInProcess(player, mob))
+            {
+                interrupt(player, mob, true);
+                res = true;
+            }
+        }
+        return res;
+    }
+
+    /**
+     * If true, the process will not be interrupted when the player dies.
+     */
+    public default boolean dontInterruptOnPlayerDie()
+    {
+        return false;
+    }
+
+    /** Indicates if the player is this mob's taming process. */
+    public boolean isInProcess(Player player, T mob);
+
+    /** Indicates if any player is in this mob's taming process. */
+    @SuppressWarnings("resource")
+    public default boolean isInProcess(T mob)
+    {
+        if (mob.level().isClientSide)
+            return false;
+        for (Player player: mob.level().players())
+        {
+            if (isInProcess(player, mob))
+                return true;
+        }
+        return false;
+    }
+
+    /** Execute when the mob attacks the player in taming process with it
+     * Requires manual invoke in subclasses by listening to events.
+     */
+    public default void onAttackProcessingPlayer(T mob, Player player, double damage)
+    {
+    }
+
+    /**
+     * Execute when the mob is attacked by the player in taming process with it.
+     * Requires manual invoke in subclasses by listening to events.
+     * */
+    public default void onAttackedByProcessingPlayer(T mob, Player player, double damage)
+    {
+    }
+
+    /**
+     * Invoked when the mob gets angry with a player.
+     */
+    public void onGettingAngry(T mob, Player player, MobAngerReason reason);
+
+    /**
+     * Get the rules about how the mob will get angry with a player.
+     */
+    public abstract MobAngerRules getAngerRules();
+
+    /**
+     * If true, the mob will not despawn if any player in the level is in process with it.
+     */
+    public static boolean persistentIfInProcess()
+    {
+        return true;
+    }
+
+    /* Util */
+    /**
+     * Do an action for all players in process.
+     * @deprecated use consumer version instead
+     */
+    @Deprecated
+    public default void forAllPlayersInProcess(T mob, BiConsumer<Player, T> todo)
+    {
+        for (Player player: mob.level().players())
+        {
+            if (isInProcess(player, mob))
+                todo.accept(player, mob);
+        }
+    }
+
+    /**
+     * Do an action for all players in process.
+     */
+    public default void forAllPlayersInProcess(T mob, Consumer<Player> todo)
+    {
+        for (Player player: mob.level().players())
+        {
+            if (isInProcess(player, mob))
+                todo.accept(player);
+        }
+    }
+
+}
--- src/main/java/net/sodiumzh/nautils/registries/NaUtilsCaps.java
+++ src/main/java/net/sodiumzh/nautils/registries/NaUtilsCaps.java
@@ -5,14 +5,31 @@ import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.common.capabilities.CapabilityToken;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.sodiumzh.nautils.capability.CEntityTickingCapability;
 import net.sodiumzh.nautils.entity.anger.CMobAngerHandler;
+import net.sodiumzh.nautils.entity.taming.CVanillaAnimalTamingProcessHandler;
+import net.sodiumzh.nautils.entity.taming.VanillaAnimalTamingProcess;
 
 public class NaUtilsCaps {
 
-    public static final Capability<CMobAngerHandler> CAP_MOB_ANGER_HANDLER = CapabilityManager.get(new CapabilityToken<>() {});
+    /**
+     * A default anger handler.
+     */
+    public static final Capability<CMobAngerHandler> CAP_MOB_DEFAULT_ANGER_HANDLER = CapabilityManager.get(new CapabilityToken<>() {});
+    /**
+     * Anger handler only for tamable animals using {@link VanillaAnimalTamingProcess}.
+     */
+    public static final Capability<CMobAngerHandler> CAP_VANILLA_TAMABLE_ANIMAL_ANGER_HANDLER = CapabilityManager.get(new CapabilityToken<>() {});
+    /**
+     * For tamable animals using taming process.
+     */
+    public static final Capability<CVanillaAnimalTamingProcessHandler> CAP_VANILLA_ANIMAL_TAMING_PROCESS_HANDLER_CAPABILITY
+            = CapabilityManager.get(new CapabilityToken<>() {});
 
     static {
-        CMobAngerHandler.register(CAP_MOB_ANGER_HANDLER);
+        CMobAngerHandler.register(CAP_MOB_DEFAULT_ANGER_HANDLER);
+        CMobAngerHandler.register(CAP_VANILLA_TAMABLE_ANIMAL_ANGER_HANDLER);
+        CEntityTickingCapability.registerTicking(CAP_VANILLA_ANIMAL_TAMING_PROCESS_HANDLER_CAPABILITY);
     }
 
     @SubscribeEvent
