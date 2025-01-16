6a427e3671a1c381cee34bf02d7b7a96fff442fa
SodiumZH <46860829+SodiumZH@users.noreply.github.com>
2025-01-17 00:29:47 +0800

    NaUtils cap templates
    Added CEntityTimerCapability (maintaining a timer)
    Added IMobSpecific

--- src/main/java/net/sodiumzh/nautils/capability/NaUtilsEntitySerializableCapProvider.java
+++ src/main/java/net/sodiumzh/nautils/capability/NaUtilsEntitySerializableCapProvider.java
@@ -5,9 +5,17 @@ import java.util.function.Supplier;
 import net.minecraft.nbt.Tag;
 import net.minecraft.world.entity.Entity;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.CapabilityProvider;
 import net.minecraftforge.common.capabilities.ICapabilitySerializable;
 import net.minecraftforge.common.util.INBTSerializable;
 
+/**
+ * A default template Capability Provider for serializable capabilities.
+ * For non-serializable caps, use {@link NaUtilsEntitySerializableCapProvider}.
+ * @param <E> Entity type using this capability
+ * @param <T> Capability interface.
+ * @param <TG> Serializing NBT type.
+ */
 public class NaUtilsEntitySerializableCapProvider<E extends Entity, T extends INBTSerializable<TG>, TG extends Tag> 
 	extends NaUtilsEntityCapProvider<E, T> implements ICapabilitySerializable<TG>
 {
