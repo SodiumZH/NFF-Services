1fe1c23609c102a1165aaff99b3618fef6321d29
SodiumZH <46860829+SodiumZH@users.noreply.github.com>
2025-01-17 00:31:55 +0800

    Removed some unused stuff

--- src/main/java/net/sodiumzh/nff/services/entity/taming/CNFFTamed.java
+++
@@ -1,9 +0,0 @@
-package net.sodiumzh.nff.services.entity.taming;
-
-import net.minecraft.nbt.CompoundTag;
-import net.minecraftforge.common.util.INBTSerializable;
-
-public interface CNFFTamed extends INFFTamed, INBTSerializable<CompoundTag>
-{
-	
-}
