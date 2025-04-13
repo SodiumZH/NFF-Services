db95497e81be4a86473cd058b81530afc17e729c
SodiumZH <46860829+SodiumZH@users.noreply.github.com>
2025-04-13 19:26:53 +0800

    Synchers
    rename: Synched Field -> Synched Getter
    now inventory is synched

--- src/main/java/net/sodiumzh/nff/services/inventory/NFFTamedMobInventory.java
+++ src/main/java/net/sodiumzh/nff/services/inventory/NFFTamedMobInventory.java
@@ -2,6 +2,7 @@ package net.sodiumzh.nff.services.inventory;
 
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.IntTag;
+import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.item.ItemStack;
@@ -10,6 +11,9 @@ import net.sodiumzh.nff.services.entity.taming.INFFTamed;
 import net.sodiumzh.nfu.util.NFUNBTStatics;
 
 import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+
 import java.util.ArrayList;
 import java.util.List;
 
