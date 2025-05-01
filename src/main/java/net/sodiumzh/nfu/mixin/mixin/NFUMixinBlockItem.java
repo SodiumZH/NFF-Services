91c1477ad1321b58bc48ca89e31b57b5d8951f55
SodiumZH <46860829+SodiumZH@users.noreply.github.com>
2025-05-02 00:27:46 +0800

    rename

--- src/main/java/net/sodiumzh/nfu/mixin/mixin/NFUMixinBlockItem.java
+++ src/main/java/net/sodiumzh/nfu/mixin/mixin/NFUMixinBlockItem.java
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(BlockItem.class)
-public class NFUBlockItemMixin implements NFUMixin<BlockItem> {
+public class NFUMixinBlockItem implements NFUMixin<BlockItem> {
 
     @WrapOperation(method = "place(Lnet/minecraft/world/item/context/BlockPlaceContext;)Lnet/minecraft/world/InteractionResult;",
         at = @At(value = "INVOKE", target = "net/minecraft/world/item/ItemStack.shrink (I)V"))
