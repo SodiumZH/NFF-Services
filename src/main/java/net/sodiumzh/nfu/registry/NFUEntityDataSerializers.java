f4b3fc4f84485aec17918601a118baac228a3e4d
SodiumZH <46860829+SodiumZH@users.noreply.github.com>
2025-07-01 01:48:11 +0800

    Misc
    Double support for EntityDataSerializer
    Enabled client operation in NFUParticleStatics

--- src/main/java/net/sodiumzh/nfu/registry/NFUEntityDataSerializers.java
+++ src/main/java/net/sodiumzh/nfu/registry/NFUEntityDataSerializers.java
@@ -1,6 +1,8 @@
 package net.sodiumzh.nfu.registry;
 
+import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.syncher.EntityDataSerializer;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -12,6 +14,19 @@ public class NFUEntityDataSerializers
 	public static final DeferredRegister<EntityDataSerializer<?>> SERIALIZERS = 
 			DeferredRegister.create(ForgeRegistries.Keys.ENTITY_DATA_SERIALIZERS, NFULibrary.MOD_ID);
 
+	public static final RegistryObject<EntityDataSerializer<Double>> DOUBLE = SERIALIZERS.register("double", () ->
+		EntityDataSerializer.simple(FriendlyByteBuf::writeDouble, FriendlyByteBuf::readDouble));
+	public static final RegistryObject<EntityDataSerializer<Vec3>> VEC3 = SERIALIZERS.register("vec3", () ->
+		EntityDataSerializer.simple((buf, val) -> {
+			buf.writeDouble(val.x);
+			buf.writeDouble(val.y);
+			buf.writeDouble(val.z);
+		}, buf -> {
+			double x = buf.readDouble();
+			double y = buf.readDouble();
+			double z = buf.readDouble();
+			return new Vec3(x, y, z);
+		}));
 	public static final RegistryObject<EntityDataSerializer<LinearColor>> LINEAR_COLOR = SERIALIZERS.register("linear_color", () -> 
 		EntityDataSerializer.simple((buf, color) -> {
 			buf.writeDouble(color.r);
