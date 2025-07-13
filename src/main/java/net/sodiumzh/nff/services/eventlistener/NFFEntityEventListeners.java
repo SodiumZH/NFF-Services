47f8110fcc839186e4b22f526a7488d2677c3008
SodiumZH <46860829+SodiumZH@users.noreply.github.com>
2025-07-14 03:13:23 +0800

    Fix death event
    Now NFFTamedDeathEvent cancellation will reset the mob health to 1

--- src/main/java/net/sodiumzh/nff/services/eventlistener/NFFEntityEventListeners.java
+++ src/main/java/net/sodiumzh/nff/services/eventlistener/NFFEntityEventListeners.java
@@ -26,7 +26,7 @@ import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.common.Mod;
-import net.sodiumzh.nfu.entity.anger.MobAngerReason;
+import net.sodiumzh.nff.services.event.entity.NFFTamedDeathEvent;
 import net.sodiumzh.nfu.entity.taming.TamingInteractionResult;
 import net.sodiumzh.nfu.mixin.event.entity.EntityDiscardEvent;
 import net.sodiumzh.nfu.mixin.event.entity.LivingStartDeathEvent;
@@ -286,6 +286,8 @@ public class NFFEntityEventListeners
 			if (event.getEntity() instanceof INFFTamed bef) {
 				if (MinecraftForge.EVENT_BUS.post(new NFFTamedDeathEvent(bef, event.getSource()))) {
 					event.setCanceled(true);
+					if (event.getEntity().getHealth() < 0.00001f)
+						event.getEntity().setHealth(1f);
 					return;
 				}
 				// Befriended mobs should not kill each other with same owner, or get killed by
