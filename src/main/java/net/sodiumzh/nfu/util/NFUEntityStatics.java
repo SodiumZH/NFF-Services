f438fd23c171d8dbc38669e39688e49c3af83f90
SodiumZH <46860829+SodiumZH@users.noreply.github.com>
2025-08-12 03:20:51 +0800

    Minor

--- src/main/java/net/sodiumzh/nfu/util/NFUEntityStatics.java
+++ src/main/java/net/sodiumzh/nfu/util/NFUEntityStatics.java
@@ -10,10 +10,8 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
@@ -22,8 +20,6 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.damagesource.DamageType;
-import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
@@ -895,18 +891,35 @@ public class NFUEntityStatics
 	}
 
 	/**
-	 * Set a mob's motion on server. Client motion will be auto synched. Do nothing on client.
+	 * Add a mob's delta motion on server. Client motion will be auto synched. Do nothing on client.
 	 */
-	public static void setMotionOnServer(Entity target, Vec3 newPos, Vec3 newVelocity) {
+	public static void addMotionOnServer(Entity target, Vec3 deltaPos, Vec3 deltaVelocity) {
 		if (target.level().isClientSide) return;
-		target.setPos(target.position().add(newPos));
-		target.addDeltaMovement(newVelocity);
-		ClientboundEntityMotionUpdatePacket packet = new ClientboundEntityMotionUpdatePacket(target.getId(), newPos, newVelocity);
+		target.setPos(target.position().add(deltaPos));
+		target.addDeltaMovement(deltaVelocity);
+		ClientboundEntityMotionUpdatePacket packet = new ClientboundEntityMotionUpdatePacket(target.getId(), deltaPos, deltaVelocity);
 		NFUNetworkStatics.sendToAllPlayers(target.level(), NFUNetworkChannels.CHANNEL, packet);
 	}
 
+	/**
+	 * Set exact position on server. Client will be auto updated. Not doing anything on client.
+	 */
 	public static void setPosOnServer(Entity target, Vec3 newPos) {
-		setMotionOnServer(target, newPos, target.getDeltaMovement());
+		addMotionOnServer(target, newPos.subtract(target.position()), Vec3.ZERO);
+	}
+
+	/**
+	 * Add a knockback on server, and sync to client.
+	 */
+	public static void knockbackOnServer(LivingEntity target, double strength, double xDirection, double zDirection) {
+		Vec3 oldVelocity = target.getDeltaMovement();
+		Vec3 oldPos = target.position();
+		target.knockback(strength, xDirection, zDirection);
+		Vec3 deltaVelocity = target.getDeltaMovement().subtract(oldVelocity);
+		Vec3 deltaPos = target.position().subtract(oldPos);
+		ClientboundEntityMotionUpdatePacket packet =
+			new ClientboundEntityMotionUpdatePacket(target.getId(), deltaPos, deltaVelocity);
+		NFUNetworkStatics.sendToAllPlayers(target.level(), NFUNetworkChannels.CHANNEL, packet);
 	}
 
 }
