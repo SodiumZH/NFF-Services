22923056294a28eb8d1f29789d3f11aa28267752
SodiumZH <46860829+SodiumZH@users.noreply.github.com>
2025-08-16 18:57:30 +0800

    Fixes
    Fixed trading issue on standalone server caused by missing mixin
    Fixed particles causing standalone server crash
    Fixed tamed animals attacking nff mobs
    Fixed `nffservices:passive_to_nff_mobs` tag making the mob passive to all mobs.

--- src/main/java/net/sodiumzh/nff/services/eventlistener/NFFEntityEventListeners.java
+++ src/main/java/net/sodiumzh/nff/services/eventlistener/NFFEntityEventListeners.java
@@ -293,9 +293,9 @@ public class NFFEntityEventListeners
 	        }
 	        // Handle Golems End
 	        // Handle hostility tags
-			if (mob.getType().is(NFFTagRegistry.PASSIVE_TO_NFF_MOBS))
+			if (mob.getType().is(NFFTagRegistry.PASSIVE_TO_NFF_MOBS) && INFFTamed.get(target).isPresent())
 				event.setCanceled(true);
-	        if (mob.getType().is(NFFTagRegistry.NEUTRAL_TO_NFF_MOBS) && target instanceof INFFTamed && mob.getLastHurtByMob() != target)
+	        if (mob.getType().is(NFFTagRegistry.NEUTRAL_TO_NFF_MOBS) && INFFTamed.get(target).filter(t -> !t.asMob().equals(mob.getLastAttacker())).isPresent())
 	        	event.setCanceled(true);
 		}
 		// Handle mobs end //
