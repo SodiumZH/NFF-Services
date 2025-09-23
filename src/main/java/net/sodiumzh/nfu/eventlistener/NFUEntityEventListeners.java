ad8d08465ac9f50355810466b58c200075bb75ce
SodiumZH <46860829+SodiumZH@users.noreply.github.com>
2025-09-24 00:37:27 +0800

    separate NFU out NO CHERRY PICK

--- src/main/java/net/sodiumzh/nfu/eventlistener/NFUEntityEventListeners.java
+++
@@ -1,9 +0,0 @@
-package net.sodiumzh.nfu.eventlistener;
-
-import net.minecraftforge.fml.common.Mod;
-import net.sodiumzh.nfu.NFULibrary;
-
-@Mod.EventBusSubscriber(modid = NFULibrary.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
-public class NFUEntityEventListeners {
-
-}
