debf5322ebc9905796cb4b63c53d94727113b0c0
SodiumZH <46860829+SodiumZH@users.noreply.github.com>
2026-06-11 02:23:09 +0800

    component-ized most capabilities

--- src/main/java/net/sodiumzh/nff/services/eventlistener/DataPort33.java
+++ src/main/java/net/sodiumzh/nff/services/eventlistener/DataPort33.java
@@ -3,8 +3,19 @@ package net.sodiumzh.nff.services.eventlistener;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.sodiumzh.nff.services.NFFServices;
+import net.sodiumzh.nff.services.entity.taming.*;
+import net.sodiumzh.nff.services.inventory.NFFTamedMobInventory;
+import net.sodiumzh.nff.services.registry.NFFDataSerializers;
+import net.sodiumzh.nff.services.registry.NFFEntityComponents;
+import net.sodiumzh.nfu.entity.component.EntityComponentAPI;
+import net.sodiumzh.nfu.mixin.event.entity.EntityFinalizeLoadingEvent;
 import net.sodiumzh.nfu.mixin.event.entity.EntityLoadEvent;
+import net.sodiumzh.nfu.network.NFUDataSerializer;
+import net.sodiumzh.nfu.network.NFUDataSerializers;
+
+import java.util.UUID;
 
 /**
  * Event listeners for porting 0.x.32 data to 0.x.33 which involves a large code clear-up.
@@ -15,10 +26,32 @@ import net.sodiumzh.nfu.mixin.event.entity.EntityLoadEvent;
 public class DataPort33 {
 
     @SubscribeEvent
-    public static void onEntityLoad(EntityLoadEvent event) {
-        CompoundTag caps = event.getNBT().getCompound("ForgeCaps");
-        CompoundTag legacyCommonData = caps.getCompound("nffservices:nff_mob_common_data");
-
+    public static void onEntityLoad(EntityFinalizeLoadingEvent event) {
+        if (INFFTamed.get(event.getEntity()).isPresent()) {
+            // Port common data
+            INFFTamed tamed = INFFTamed.get(event.getEntity()).get();
+            CompoundTag caps = event.getNBT().getCompound("ForgeCaps");
+            CompoundTag legacyCommonData = caps.getCompound("nffservices:nff_mob_common_data");
+            if (!legacyCommonData.isEmpty()) {
+                CNFFTamedCommonData.Values temp = new CNFFTamedCommonData.Values(tamed);
+                temp.deserializeNBT(legacyCommonData);
+                NFFTamedSyncherComponent syncher = EntityComponentAPI.getComponentByPath(event.getEntity(), "/nff/tamed/syncher", NFFEntityComponents.TAMED_SYNCHER.get()).orElseThrow();
+                NFFTamedDataComponent data = EntityComponentAPI.getComponentByPath(event.getEntity(), "/nff/tamed/data", NFFEntityComponents.TAMED_DATA.get()).orElseThrow();
+                NFFTamedDataAccessor accessor = tamed.getDataAccessor();
+                // Port nbt
+                temp.getAdditionalNBT().getAllKeys().forEach(k -> data.getNBT().put(k, temp.getAdditionalNBT().get(k).copy()));
+                // Port synched values
+                syncher.setSynchedData("identifier", UUID.class, temp.getIdentifier());
+                accessor.setEncounteredDate(temp.getEncounteredDate());
+                accessor.setOwnerUUID(temp.getOwnerUUID());
+                accessor.setOwnerName(temp.getOwnerName());
+                accessor.setAIState(temp.getAIState());
+                // Port other values
+                data.putPermanentVariable("initialType", ForgeRegistries.ENTITY_TYPES.getKey(temp.getInitialEntityType()), NFUDataSerializers.RESOURCE_LOCATION);
+                data.putPermanentVariable("randomStrollAnchor", temp.getAnchor(), NFUDataSerializers.VEC3);
+                data.putPermanentVariable("inventory", temp.getAdditionalInventory(), NFFDataSerializers.TAMED_MOB_INVENTORY.get());
+            }
+        }
     }
 
 }
--- src/main/java/net/sodiumzh/nff/services/registry/NFFCapRegistry.java
+++ src/main/java/net/sodiumzh/nff/services/registry/NFFCapRegistry.java
@@ -5,28 +5,18 @@ import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.common.capabilities.CapabilityToken;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.sodiumzh.nff.services.entity.capability.CAttributeMonitor;
-import net.sodiumzh.nff.services.entity.capability.CHealingHandler;
-import net.sodiumzh.nff.services.entity.capability.CLivingEntityDelayedActionHandler;
-import net.sodiumzh.nff.services.entity.capability.CNFFPlayerModule;
-import net.sodiumzh.nff.services.entity.taming.CNFFTamable;
 import net.sodiumzh.nff.services.entity.taming.CNFFTamedCommonData;
-import net.sodiumzh.nff.services.item.capability.CItemStackMonitor;
 import net.sodiumzh.nff.services.level.CNFFLevelModule;
 import net.sodiumzh.nfu.capability.CEntityTickingCapability;
 
 public class NFFCapRegistry {
 
-	public static final Capability<CItemStackMonitor> CAP_ITEM_STACK_MONITOR = CapabilityManager.get(new CapabilityToken<>(){});
-	
 	// Caps for data storage only
 	@Deprecated
 	public static final Capability<CNFFTamedCommonData> CAP_BEFRIENDED_MOB_DATA = CapabilityManager.get(new CapabilityToken<>(){});
 	
 	static {
 		CEntityTickingCapability.registerTicking(CAP_BEFRIENDED_MOB_DATA);
-		CEntityTickingCapability.registerTicking(CAP_ITEM_STACK_MONITOR);
-		//CMobAngerHandler.register(CAP_BEFRIENDABLE_MOB);
 	}
 	
 	@SubscribeEvent
